3926b330ff45ed6c8e5bae9373757ba8bf8215fd
Iaroslav Popov <iaroslav.popov@zalando.de>
2016-11-23 14:08:29 +0100

    Incorporated wycat dbmon repo

---
+++ tests/unit/utils/get-data-test.js
@@ -0,0 +1,10 @@
+import getData from 'js-repaint-perfs-ember-2-9-1/utils/get-data';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get data');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getData();
+  assert.ok(result);
+});
